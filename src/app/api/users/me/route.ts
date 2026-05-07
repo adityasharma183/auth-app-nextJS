d2e235518212f87cb0c2ff5767caa49a443bb556
Aditya Sharma <adityaa.sharma9757@gmail.com>
2026-05-07 15:13:46 +0530

    added middleware and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,27 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+
+connect()
+
+export async function GET(request: NextRequest) {
+    try {
+        const userId = await getDataFromToken(request)
+
+        if (!userId) {
+            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+        }
+
+        const user = await User.findById(userId).select('-password')
+
+        if (!user) {
+            return NextResponse.json({ error: 'User not found' }, { status: 404 })
+        }
+
+        return NextResponse.json({ user }, { status: 200 })
+    } catch (error:any) {
+        console.error('Error fetching user data:', error)
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
